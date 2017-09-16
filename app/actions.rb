6c20849d9fc8f974a024048c0543a1421e151f3a
peter born <peterborn1726@gmail.com>
2017-09-16 23:32:57 +0000

    ruby

--- app/actions.rb
+++ app/actions.rb
@@ -1,27 +1,24 @@
-get '/' do
-    username = "sharky_j"
-    avatar_url = "http://naserca.com/images/sharky_j.jpg"
-    photo_url = "http://naserca.com/images/shark.jpg"
-    time_ago_in_minutes = 60
-    like_count = 0
-    comment_count = 1
-    comments = [
-        "sharky_j: Out for the long weekend... to embarrassed to show ya'll the beach bod!"
-    ]
-    #if the time_ago_in_minutes is more than 60
-    if time_ago_in_minutes > 60
-        
-        #return this string
-        "more than an hour"
-        
-    elsif time_ago_in_minutes == 60
-       
-        "an hour"
-    
-    #if it's less than or equal
-    else
-        
-        #return this instead
-        "less than an hour"
+def humanized_time_ago(time_ago_in_minutes)
+        if time_ago_in_minutes >= 60
+            "#{time_ago_in_minutes / 60} hours ago"
+        else
+            "#{time_ago_in_minutes} minutes ago"
     end
+end
+
+get '/' do
+   post = {
+    username: "sharky_j",
+    avatar_url = "http://naserca.com/images/sharky_j.jpg",
+    humanized_time_ago: humanized_time_ago(15),
+    photo_url = "http://naserca.com/images/shark.jpg",
+    time_ago_in_minutes: 15,
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+        "sharky_j: Out for the long weekend... to embarrassed to show ya'll the beach bod!"
+    }]
+   }
+    
+    
 end
