8a77470c7b393eb9920b8d8a1d7f16d73c9d136e
peter born <peterborn1726@gmail.com>
2017-09-18 03:08:47 +0000

    ruby

--- app/actions.rb
+++ app/actions.rb
@@ -6,19 +6,56 @@ def humanized_time_ago(time_ago_in_minutes)
     end
 end
 
+
+
 get '/' do
-   post = {
+  
+  # ...
+  
+  
+   post_shark = {
     username: "sharky_j",
-    avatar_url = "http://naserca.com/images/sharky_j.jpg",
+    avatar_url: "http://naserca.com/images/sharky_j.jpg",
+    photo_url: "http://naserca.com/images/shark.jpg",
     humanized_time_ago: humanized_time_ago(15),
-    photo_url = "http://naserca.com/images/shark.jpg",
-    time_ago_in_minutes: 15,
     like_count: 0,
     comment_count: 1,
     comments: [{
-        "sharky_j: Out for the long weekend... to embarrassed to show ya'll the beach bod!"
+       username: "sharky_j",
+       text: "Out for the long weekend... too embarrassed to show ya'll the beach bod!"
     }]
    }
     
+    humanized_time_ago(post[time_ago_in_minutes])
     
+   post_whale = {
+    username: "kirk_whalum",
+    avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+    photo_url: "http://naserca.com/images/whale.jpg",
+    humanized_time_ago: humanized_time_ago(65),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+       username: "kirk_whalum",
+       text: "#weekendvibes"
+    }]
+   }
+    
+    humanized_time_ago(post[time_ago_in_minutes])
+    
+   post_marlin = {
+    username: "marlin_peppa",
+    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+    photo_url: "http://naserca.com/images/marlin.jpg",
+    humanized_time_ago: humanized_time_ago(190),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+       username: "marlin_peppa",
+       text: "lunchtime! ;)"
+    }]
+   }
+   
+   [post_shark, post_whale, post_marlin].to_s
+   
 end
