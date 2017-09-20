1f4ca68014245d268e6a6e955c9451ab39d4540b
peter born <peterborn1726@gmail.com>
2017-09-20 04:46:16 +0000

    erb newbie

--- app/actions.rb
+++ app/actions.rb
@@ -1,15 +1,14 @@
 get '/' do
-  
-  # ...
-     def humanized_time_ago(time_ago_in_minutes)
-        if time_ago_in_minutes >= 60
-            "#{time_ago_in_minutes / 60} hours ago"
-        else
-            "#{time_ago_in_minutes} minutes ago"
-        end
+ 
+   def humanized_time_ago(time_ago_in_minutes)
+     if time_ago_in_minutes >=60
+      "#{time_ago_in_minutes / 60} hours ago"
+     else
+      "#{time_ago_in_minutes} minutes ago"
      end
-  
-   post_shark = {
+   end
+   
+   @post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -23,7 +22,7 @@ get '/' do
    }
     
     
-   post_whale = {
+   @post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -37,7 +36,7 @@ get '/' do
    }
     
     
-   post_marlin = {
+   @post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -50,7 +49,8 @@ get '/' do
     }]
    }
    
-   [post_shark, post_whale, post_marlin].to_s
+   @posts = [@post_shark, @post_whale, @post_marlin]
    
-  erb :index
+   
+  erb (:index)
 end
